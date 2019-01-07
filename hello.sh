3493dd806640ba5d1930327e4d09ee8f332ae43b
walter-a-morton <walter.a.morton@intel.com>
2019-01-07 16:16:26 -0700

    Adding Jmeter calls

--- hello.sh
+++ hello.sh
@@ -2,7 +2,8 @@
 echo "<> Start <>"
 pwd
 date
-sh /home/wam/apache-jmeter-5.0/bin/jmeter.sh -n -t test-one.jmx -l output_event_test.csv
-cp output_event_test.csv /home/wam/data/$(date +%s)_output_event_test.csv
+sh /home/wam/apache-jmeter-5.0/bin/jmeter.sh -n -t test-one.jmx -l $(date +%s)_output_event_test.csv
+#sh /home/wam/apache-jmeter-5.0/bin/jmeter.sh -n -t test-one.jmx -l output_event_test.csv
+#cp output_event_test.csv /home/wam/data/$(date +%s)_output_event_test.csv
 echo "<> All Done <>"
 
